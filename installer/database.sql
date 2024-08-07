603302eaeee1140527d41f61a4f343c11f6321da
Amritesh Kumar <amritesh.kr8@gmail.com>
2024-08-07 11:49:12 +0530

    db permissions fixed

--- installer/database.sql
+++ installer/database.sql
@@ -2,34 +2,34 @@ CREATE DATABASE IF NOT EXISTS hyperef;
 USE hyperef;
 
 CREATE TABLE admins (
-    name VARCHAR(20) DEFAULT NULL,
-    password VARCHAR(30) DEFAULT NULL
+    name VARCHAR(20) NOT NULL,
+    password VARCHAR(30) NOT NULL
 );
 
 
 CREATE TABLE questions (
     id INT(11) NOT NULL PRIMARY KEY,
-    question TEXT DEFAULT NULL,
+    question TEXT NOT NULL,
     score INT(11) DEFAULT 0
 );
 
 CREATE TABLE submissions (
-    schoolcode VARCHAR(50) DEFAULT NULL,
+    schoolcode VARCHAR(50) NOT NULL,
     time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    question INT(11) DEFAULT NULL,
-    score int(255) DEFAULT NULL
+    question INT(11) NOT NULL,
+    score int(255) NOT NULL
 );
 
 CREATE TABLE users (
-    schoolcode VARCHAR(15) DEFAULT NULL,
-    password VARCHAR(20) DEFAULT NULL,
-    school VARCHAR(50) DEFAULT NULL
+    schoolcode VARCHAR(15) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    school VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE testcases (
     id int(32) NOT NULL,
-    input VARCHAR(1024) DEFAULT NULL,
-    output VARCHAR(1024) DEFAULT NULL
+    input VARCHAR(1024) NOT NULL,
+    output VARCHAR(1024) NOT NULL
 );
 
 insert into admins(name,password) values('admin','admin@!#+330'); -- Change the username and password for the web panel
